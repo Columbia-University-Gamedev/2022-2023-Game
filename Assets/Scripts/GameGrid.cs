c8624ebc5e3518d3eec5ed2124fe10699e598219
Ryan Xu <ryan-xu@live.com>
2022-10-08 13:30:20 -0400

    tile class

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    public Dictionary<Tile.TileType, int> pathfindingCosts;
+    public int width, height;
+    public Tile[,] grid;
+
+    void Start()
+    {
+        grid = new Tile[width, height];
+    }
+}
+
+public class Tile
+{
+    public static Dictionary<TileType, int> cost;
+    
+    public TileType tile_type = TileType.Plain;
+    public bool destroyed = false;
+
+    public int GetCost()
+    {
+        return cost[tile_type];
+    }
+
+    public enum TileType
+    {
+        Plain = 0,
+        City = 1,
+    }
+}
